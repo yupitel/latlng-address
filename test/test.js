508e074f882c39b53d5e07ca5b0ed826fd49a211
yupitel <yupitel@yupitelnoMac-mini.local>
2017-10-18 02:31:43 +0900

    add test file. add circle ci file

---
+++ test/test.js
@@ -0,0 +1,30 @@
+const LatLngAdr = require('../index.js');
+const expect    = require('chai').expect;
+
+describe('Library test', () => {
+  before(() => null);
+
+  describe('Get request session', () => {
+    it('get list of config files', (done) => {
+      const config = {
+        key: process.env.KEY,
+        stagger_time: 1000,
+        encode_polylines: false,
+        secure: true
+      };
+      console.log(config);
+      const lla = new LatLngAdr(config);
+      lla.getAddress(40.782906, -73.965433)
+      .then((address) => {
+        console.log(address);
+        expect(address).to.not.be.a('null');
+        done();
+      });
+    });
+  });
+  after(() => {
+    console.log('finish.');
+  });
+});
+
+
